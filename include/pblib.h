de2d6ebed9cddfb70165d5a009bbd241a255bb81
Filipi565 <filipi565@hotmail.com>
2024-11-17 20:37:41 -0300

    Basic include file

---
+++ include/pblib.h
@@ -0,0 +1,27 @@
+#ifndef _LIB_H_
+#define _LIB_H_
+
+#include <raylib.h>
+
+#ifdef PB_LIB_EXPORT
+#define PB_API __declspec(dllexport)
+#else
+#define PB_API __declspec(dllimport)
+#endif
+
+namespace PixelBattle
+{
+    extern int WinHeight;
+    extern int WinWidth;
+
+    class Entity
+    {
+        public:
+        PB_API Entity();
+        PB_API Entity(float x, float y);
+
+        Vector2 pos;
+    };
+}
+
+#endif
