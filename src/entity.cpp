5c2002924435ebb1e733cfc547b3bf8ec275e71f
Filipi565 <filipi565@hotmail.com>
2024-11-17 20:39:14 -0300

    Basic entity source

---
+++ src/entity.cpp
@@ -0,0 +1,15 @@
+#include "pbconfig.h"
+#include <pblib.h>
+
+namespace PixelBattle
+{
+    PB_API Entity::Entity()
+    {
+        pos = {0, 0};
+    }
+
+    PB_API Entity::Entity(float x, float y)
+    {
+        pos = {x, y};
+    }
+}
