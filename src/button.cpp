f5b71c9b5f2cf49a40e33448c68a7944eb12abb4
Filipi565 <filipi565@hotmail.com>
2024-11-21 19:54:29 -0300

    Add a button click

--- src/button.cpp
+++ src/button.cpp
@@ -1,14 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pblib.h>
+#include <string>
+
+using std::string;
 
 namespace PixelBattle
 {
+    Sound *button_click_effect = nullptr;
+
     Button::Button() : TextureObject()
     {
         visible = false;
         size = {0, 0};
         pos = {0, 0};
+
+        if (button_click_effect == nullptr)
+        {
+            string result = assets;
+            result += "/sounds/ButtonClick.wav";
+
+            button_click_effect = new Sound();
+            (*button_click_effect) = LoadSound(result.data());
+        }
 
         OnClick = &_M_NoPointer;
     }
@@ -22,6 +36,7 @@ namespace PixelBattle
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && in_colision && visible)
         {
             this->OnClick();
+            PlaySound(*button_click_effect);
         }
 
         Rectangle source = {0, 0, (float)texture->width, (float)texture->height};
