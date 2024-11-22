2c42fedd1796556237d2a6987cce818fb9c3a971
Filipi565 <filipi565@hotmail.com>
2024-11-21 23:19:37 -0300

    Objects will not be null

--- src/clean.cpp
+++ src/clean.cpp
@@ -7,37 +7,16 @@ namespace PixelBattle
     void Clean(void)
     {
         using namespace StartMenu;
-        if (sound != nullptr)
-        {
-            UnloadSound(*sound);
-            delete sound;
-        }
 
-        if (background != nullptr)
-        {
-            UnloadTexture(*background);
-            delete background;
-        }
+        UnloadSound(*sound);
+        UnloadTexture(*background);
+        UnloadSound(*button_click_effect);
 
-        if (play_button != nullptr)
-        {
-            delete play_button;
-        }
-
-        if (settings_button != nullptr)
-        {
-            delete settings_button;
-        }
-
-        if (extras_button != nullptr)
-        {
-            delete extras_button;
-        }
-
-        if (button_click_effect != nullptr)
-        {
-            UnloadSound(*button_click_effect);
-            delete button_click_effect;
-        }
+        delete button_click_effect;
+        delete settings_button;
+        delete extras_button;
+        delete play_button;
+        delete background;
+        delete sound;
     }
 }
