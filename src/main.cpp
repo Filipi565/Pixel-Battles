482f9036a2c610294ed92b4428f5036bc194868d
Filipi565 <filipi565@hotmail.com>
2024-11-22 19:34:06 -0300

    Put the game loop outside the main function

--- src/main.cpp
+++ src/main.cpp
@@ -13,75 +13,83 @@ namespace PixelBattle
 using namespace PixelBattle;
 using std::string;
 
+namespace Application
+{
+    int Run(int &argc, const char *argv[])
+    {
+        if (!(argc >= 2 && argv[1] == string("--hide-console=off")))
+        {
+            HideConsole();
+        }
+        
+        InitWindow(0, 0, "Pixel Battle");
+
+        executable = argv[0];
+        char *assets_path = GetAssetsPath();
+        assets = assets_path; // Set it globaly
+
+        InitAudioDevice();
+
+        ToggleFullscreen();
+
+        Entity player;
+        player.speed = 10;
+        player.color = WHITE;
+        player.pos = {GetScreenWidth()/2.0f, GetScreenHeight()/2.0f};
+
+        SetTargetFPS(60);
+
+        // Load all Assets
+        std::thread _loader = std::thread(LoadAssets);
+
+        while (!is_game_loaded)
+        {
+            BeginDrawing();
+            ClearBackground(BLACK);
+            DrawText("Loading...", 10, 20, 30, WHITE);
+            EndDrawing();
+        }
+
+        // Load all Textures from the images
+        // Note: textures can not be loaded outside the main thread
+        LoadTextures();
+
+        while (!WindowShouldClose())
+        {
+            if (!IsCursorOnScreen() && GetTime() >= 5 && !IsWindowMinimized())
+            {
+                MinimizeWindow();
+            }
+
+            BeginDrawing();
+
+            if (on_start_menu)
+            {
+                LoadStartMenu();
+                EndDrawing();
+                continue;
+            }
+
+            ClearBackground(BLACK);
+
+            player.Update();
+
+            EndDrawing();
+        }
+
+        delete[] assets_path;
+
+        UnloadAssets();
+
+        CloseAudioDevice();
+        CloseWindow();
+
+        return 0;
+    }
+}
+
 // On my pc using WinMain does not work propely
 int main(int argc, const char *argv[])
 {
-    if (!(argc >= 2 && argv[1] == string("--hide-console=off")))
-    {
-        HideConsole();
-    }
-    
-    InitWindow(0, 0, "Pixel Battle");
-
-    executable = argv[0];
-    char *assets_path = GetAssetsPath();
-    assets = assets_path; // Set it globaly
-
-    InitAudioDevice();
-
-    ToggleFullscreen();
-
-    Entity player;
-    player.speed = 10;
-    player.color = WHITE;
-    player.pos = {GetScreenWidth()/2.0f, GetScreenHeight()/2.0f};
-
-    SetTargetFPS(60);
-
-    // Load all Assets
-    std::thread _loader = std::thread(LoadAssets);
-
-    while (!is_game_loaded)
-    {
-        BeginDrawing();
-        ClearBackground(BLACK);
-        DrawText("Loading...", 10, 20, 30, WHITE);
-        EndDrawing();
-    }
-
-    // Load all Textures from the images
-    // Note: textures can not be loaded outside the main thread
-    LoadTextures();
-
-    while (!WindowShouldClose())
-    {
-        if (!IsCursorOnScreen() && GetTime() >= 5 && !IsWindowMinimized())
-        {
-            MinimizeWindow();
-        }
-
-        BeginDrawing();
-
-        if (on_start_menu)
-        {
-            LoadStartMenu();
-            EndDrawing();
-            continue;
-        }
-
-        ClearBackground(BLACK);
-
-        player.Update();
-
-        EndDrawing();
-    }
-
-    delete[] assets_path;
-
-    UnloadAssets();
-
-    CloseAudioDevice();
-    CloseWindow();
-
-    return 0;
+    return Application::Run(argc, argv);
 }
